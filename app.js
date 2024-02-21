7398e94b1ef9cf4609b66c911adf4733442f00c9
SarimEffendi <59098486+SarimEffendi@users.noreply.github.com>
2024-02-21 22:19:16 +0500

    update adminController

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const connectDb = require('./config/dbConnection');
 require('dotenv').config();
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 app.use(express.json());
 connectDb();
 
+app.use('/admin', adminRoutes);
+
+
+
 require('./utils/routes')(app);
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}...`));
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,63 +1,83 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
 const Admin = require('../models/adminModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const registerAdmin = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
-    if(!username || !email || !password){
+    if (!username || !email || !password) {
         res.status(400);
         throw new Error("All fields are required");
     }
-    const adminAvailable = await User.findOne({email});
 
-    if(adminAvailable){
+    const userAvailable = await User.findOne({ email });
+
+    if (userAvailable) {
         res.status(400);
-        throw new Error("Admin already registered");
+        throw new Error("Email address already registered");
     }
 
-    // Hashed Pass
     const hashedPass = await bcrypt.hash(password, 10);
-    const admin = await Admin.create({
-       username: username, email: email, password: hashedPass
+
+    const user = await User.create({
+        username: username,
+        email: email,
+        password: hashedPass,
+        role: 'admin'  
     });
-    if(admin){
-        res.status(201).json({_id: admin.id, email: admin.email});   
-    }
-    else{
+
+    const admin = await Admin.create({
+        user_id: user._id,  
+        fullname: req.body.fullname,
+        contact: req.body.contact
+    });
+
+    if (admin) {
+        res.status(201).json({ _id: admin._id, email: admin.user_id.email });
+    } else {
         res.status(400);
         throw new Error("Admin data not valid");
     }
-    res.json({message: "Registered successfully"});
 });
 
 const loginAdmin = asyncHandler(async (req, res) => {
-    const {email, password} = req.body;
-    if(!email || !password){
+    const { email, password } = req.body;
+    if (!email || !password) {
         res.status(400);
         throw new Error("All fields are required");
     }
-    const admin = await Admin.findOne({email});
-    if(admin && (await bcrypt.compare(password, admin.password))){
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const admin = await Admin.findOne({ user_id: user._id });
+        if (!admin) {
+            res.status(401);
+            throw new Error("Admin data not found");
+        }
+
         const accessToken = jwt.sign({
-                admin: {
-                    username: admin.username,
-                    email: admin.email,
-                    id: admin.id,
-                },
-            }, process.env.ACCESS_TOKEN_SECRET,
-            {expiresIn: "365d"}
+            admin: {
+                username: user.username,
+                email: user.email,
+                id: user._id,
+                fullname: admin.fullname,
+                contact: admin.contact,
+            },
+        }, process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "365d" }
         );
-        res.status(200).json({accessToken});
-    }
-    else{
+        res.status(200).json({ accessToken });
+    } else {
         res.status(401);
         throw new Error("Email or password is not valid.");
     }
 });
 
+
 const currentAdmin = asyncHandler(async (req, res) => {
     res.json(req.admin);
 });
 
-module.exports = {registerAdmin, loginAdmin, currentAdmin};
+module.exports = { registerAdmin, loginAdmin, currentAdmin };
